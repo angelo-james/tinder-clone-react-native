94f1959b297fbde9784de2d1521f9728a382a6e0
AJ Arriola <angelojames.arriola@gmail.com>
2019-03-25 10:17:19 -0700

    removed some styling from App.js file and added a Animated component from Ball.js with a componentwillmount lifecycle method to move the ball from one position to another

--- src/comonents/Ball.js
+++ src/comonents/Ball.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+
+    Animated.spring(this.position, {
+      toValue: {x: 200, y: 500}
+    }).start()
+  };
+
   render() {
     return (
-      <View style={styles.ball} />
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
     )
   }
 };
 
 const styles = {
   ball: {
-    height: 60,
+    height: 60, 
     width: 60,
     borderRadius: 30,
     borderWidth: 30,
